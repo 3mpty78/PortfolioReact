f490d96be35c2f3f88eabbea5b9e5290d734a0be
3mpty <aurelien.corre@outlook.fr>
2023-10-17 22:26:38 +0200

    Nodemailer config

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,53 @@
 const express = require("express");
-const transporter = require("./nodemailer-config");
+const dotenv = require("dotenv");
+const nodemailer = require("nodemailer");
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
 
-app.post("/envoyer-email", (req, res) => {
-      const { email, message } = req.body;
+// Route pour envoyer le formulaire de contact
+app.post("/envoyer-email", async (req, res) => {
+      try {
+            const { email, message } = req.body;
 
-      const mailOptions = {
-            from: process.env.EMAIL,
-            to: process.env.EMAIL, // Ton e-mail personnel
-            subject: "Nouveau message de contact depuis le Portfolio !",
-            text: `Email: ${email}\nMessage: ${message}`,
-      };
+            // Création du transporter nodemailer
+            const transporter = nodemailer.createTransport({
+                  service: "Gmail",
+                  auth: {
+                        user: process.env.EMAIL,
+                        pass: process.env.PASSWORD,
+                  },
+            });
 
-      transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                  console.error(
-                        "Erreur lors de l'envoi de l'e-mail : " + error
-                  );
-                  res.status(500).json({
-                        message: "Erreur lors de l'envoi de l'e-mail",
-                  });
-            } else {
-                  console.log("E-mail envoyé avec succès !", info);
-                  res.json({ message: "E-mail envoyé avec succès !" });
-            }
-      });
+            // Options du mail
+            const mailOptions = {
+                  from: "Portfolio contact",
+                  to: process.env.EMAIL, // Ton adresse e-mail spécifiée dans .env
+                  subject: `Formulaire de contact du Portfolio`,
+                  text: `
+                        Email : ${email}
+                        Message : ${message}
+                  `,
+            };
+
+            // Envoie du mail
+            await transporter.sendMail(mailOptions);
+
+            res.status(200).json({
+                  message: "Formulaire de contact envoyé avec succès",
+            });
+      } catch (error) {
+            console.error(
+                  "Erreur lors de l'envoi du formulaire de contact :",
+                  error
+            );
+            res.status(500).json({
+                  message: "Une erreur est survenue lors de l'envoi du formulaire de contact",
+            });
+      }
 });
 
 app.listen(port, () => {
