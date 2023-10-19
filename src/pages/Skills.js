3dd07168aeafe4ae745a6968b4261973c86ada34
3mpty <aurelien.corre@outlook.fr>
2023-10-19 20:06:13 +0200

    Added Marquee component to skills section

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 // Import des icons
 import figma from "../assets/icons/tech/figma.svg";
@@ -23,67 +24,103 @@ export default function Skills() {
 
                   <div className="banner-container">
                         <div className="banner">
-                              <h3>Design</h3>
-                              <ul className="design list">
-                                    <li>
-                                          <img src={figma} alt="Figma icon" />
-                                          <p>Figma</p>
-                                    </li>
-                                    <li>
-                                          <img
-                                                src={illustrator}
-                                                alt="Adobe Illustrator icon"
-                                          />
-                                          <p>Illustrator</p>
-                                    </li>
-                                    <li>
-                                          <img
-                                                src={photoshop}
-                                                alt="Adobe Photoshop icon"
-                                          />
-                                          <p>Photoshop</p>
-                                    </li>
-                              </ul>
+                              <h3>Compétences Design</h3>
+                              <Marquee
+                                    play={true}
+                                    pauseOnHover={true}
+                                    direction="left"
+                                    speed={100}
+                                    loop={0}>
+                                    <ul className="design list">
+                                          <li>
+                                                <img
+                                                      src={figma}
+                                                      alt="Figma icon"
+                                                />
+                                                <p>Figma</p>
+                                          </li>
+                                          <li>
+                                                <img
+                                                      src={illustrator}
+                                                      alt="Adobe Illustrator icon"
+                                                />
+                                                <p>Illustrator</p>
+                                          </li>
+                                          <li>
+                                                <img
+                                                      src={photoshop}
+                                                      alt="Adobe Photoshop icon"
+                                                />
+                                                <p>Photoshop</p>
+                                          </li>
+                                    </ul>
+                              </Marquee>
                         </div>
                         <div className="banner">
-                              <h3>Web</h3>
-                              <ul className="web list">
-                                    <li>
-                                          <img src={html} alt="HTML 5 icon" />
-                                          <p>HTML</p>
-                                    </li>
-                                    <li>
-                                          <img src={css} alt="CSS 3 icon" />
-                                          <p>CSS</p>
-                                    </li>
-                                    <li>
-                                          <img src={js} alt="Javascript icon" />
-                                          <p>Javascript</p>
-                                    </li>
-                              </ul>
+                              <h3>Compétences Web</h3>
+                              <Marquee
+                                    play={true}
+                                    pauseOnHover={true}
+                                    direction="right"
+                                    speed={50}
+                                    loop={0}>
+                                    <ul className="web list">
+                                          <li>
+                                                <img
+                                                      src={html}
+                                                      alt="HTML 5 icon"
+                                                />
+                                                <p>HTML</p>
+                                          </li>
+                                          <li>
+                                                <img
+                                                      src={css}
+                                                      alt="CSS 3 icon"
+                                                />
+                                                <p>CSS</p>
+                                          </li>
+                                          <li>
+                                                <img
+                                                      src={js}
+                                                      alt="Javascript icon"
+                                                />
+                                                <p>Javascript</p>
+                                          </li>
+                                    </ul>
+                              </Marquee>
                         </div>
                         <div className="banner">
-                              <h3>J'apprends</h3>
-                              <ul className="learning list">
-                                    <li>
-                                          <img
-                                                src={react}
-                                                alt="React JS icon"
-                                          />
-                                          <p>React JS</p>
-                                    </li>
-                                    <li>
-                                          <img
-                                                src={react}
-                                                alt="React Native icon"
-                                          />
-                                          <p>React Native</p>
-                                    </li>
-                                    <li>
-                                          <img src={node} alt="Node JS icon" />
-                                          <p>Node JS</p>
-                                    </li>
-                              </ul>
+                              <h3>J'apprends </h3>
+                              <Marquee
+                                    play={true}
+                                    pauseOnHover={true}
+                                    direction="left"
+                                    speed={70}
+                                    loop={0}>
+                                    <ul className="learning list">
+                                          <li>
+                                                <img
+                                                      src={react}
+                                                      alt="React JS icon"
+                                                />
+                                                <p>React JS</p>
+                                          </li>
+                                          <li>
+                                                <img
+                                                      src={react}
+                                                      alt="React Native icon"
+                                                />
+                                                <p>React Native</p>
+                                          </li>
+                                          <li>
+                                                <img
+                                                      src={node}
+                                                      alt="Node JS icon"
+                                                />
+                                                <p>Node JS</p>
+                                          </li>
+                                    </ul>
+                              </Marquee>
                         </div>
                   </div>
             </div>
