9a5583bd4d80de2fc4bde0b333ec686efc1fa14d
3mpty <aurelien.corre@outlook.fr>
2023-10-19 19:51:12 +0200

    Added SVG icons for technos

--- src/components/Sphere.js
+++ src/components/Sphere.js
@@ -10,37 +10,8 @@ const CursorSphere = () => {
                   setDotPosition({ x: e.clientX - 2.5, y: e.clientY - 15 });
             };
 
-            // Récupération des cartes
-            const card1 = document.getElementById("card 0");
-            const card2 = document.getElementById("card 1");
-            const card3 = document.getElementById("card 2");
-
             // Création des events
             window.addEventListener("mousemove", handleMouseMove);
-
-            // Effet pour la 1ere carte
-            card1.addEventListener("mouseenter", () => {
-                  card1.style.boxShadow = "0 0 20px hsla(300, 100%, 50%, 0.25)";
-            });
-            card1.addEventListener("mouseleave", () => {
-                  card1.style.boxShadow = "none";
-            });
-
-            // Effet pour la 2eme carte
-            card2.addEventListener("mouseenter", () => {
-                  card2.style.boxShadow = "0 0 20px hsla(16, 100%, 50%, 0.25)";
-            });
-            card2.addEventListener("mouseleave", () => {
-                  card2.style.boxShadow = "none";
-            });
-
-            // Effet pour la 3eme carte
-            card3.addEventListener("mouseenter", () => {
-                  card3.style.boxShadow = "0 0 20px hsla(0, 0%, 50%, 0.25)";
-            });
-            card3.addEventListener("mouseleave", () => {
-                  card3.style.boxShadow = "none";
-            });
 
             return () => {
                   window.removeEventListener("mousemove", handleMouseMove);
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,85 +1,90 @@
 import React from "react";
 
-const cards = [
-      {
-            name: "Design",
-            technos: ["Canva", "Photoshop", "Illustrator"],
-      },
-      {
-            name: "Web",
-            technos: ["HTML", "CSS", "Javascript"],
-      },
-      {
-            name: "J'apprends...",
-            technos: ["React", "Node.js", "React Native"],
-      },
-];
+// Import des icons
+import figma from "../assets/icons/tech/figma.svg";
+import illustrator from "../assets/icons/tech/illustrator.svg";
+import photoshop from "../assets/icons/tech/photoshop.svg";
+import html from "../assets/icons/tech/html.svg";
+import css from "../assets/icons/tech/css.svg";
+import js from "../assets/icons/tech/js.svg";
+import react from "../assets/icons/tech/react.svg";
+import node from "../assets/icons/tech/node.svg";
 
 export default function Skills() {
+      const lists = document.querySelectorAll(".list");
+
+      lists.forEach((list) => {
+            list.innerHTML += list.innerHTML;
+      });
+
       return (
             <div id="skills">
                   <h2>Skills</h2>
-                  <div className="cards_container">
-                        {cards.map((card, index) => (
-                              <div className={`card`} id={`card ${index}`}>
-                                    <h3>{card.name}</h3>
-                                    <div className="border"></div>
-                                    <div className="technos">
-                                          <span
-                                                className={`icon ${
-                                                      card.technos[0] ===
-                                                      "Canva"
-                                                            ? "canva"
-                                                            : card
-                                                                    .technos[0] ===
-                                                              "HTML"
-                                                            ? "html"
-                                                            : card
-                                                                    .technos[0] ===
-                                                              "React"
-                                                            ? "react"
-                                                            : ""
-                                                }`}></span>
-                                          <p>{card.technos[0]}</p>
-                                    </div>
-                                    <div className="technos">
-                                          <span
-                                                className={`icon ${
-                                                      card.technos[1] ===
-                                                      "Photoshop"
-                                                            ? "photoshop"
-                                                            : card
-                                                                    .technos[1] ===
-                                                              "CSS"
-                                                            ? "css"
-                                                            : card
-                                                                    .technos[1] ===
-                                                              "Node.js"
-                                                            ? "node"
-                                                            : ""
-                                                }`}></span>
-                                          <p>{card.technos[1]}</p>
-                                    </div>
-                                    <div className="technos">
-                                          <span
-                                                className={`icon ${
-                                                      card.technos[2] ===
-                                                      "Illustrator"
-                                                            ? "illustrator"
-                                                            : card
-                                                                    .technos[2] ===
-                                                              "Javascript"
-                                                            ? "javascript"
-                                                            : card
-                                                                    .technos[2] ===
-                                                              "React Native"
-                                                            ? "react"
-                                                            : ""
-                                                }`}></span>
-                                          <p>{card.technos[2]}</p>
-                                    </div>
-                              </div>
-                        ))}
+
+                  <div className="banner-container">
+                        <div className="banner">
+                              <h3>Design</h3>
+                              <ul className="design list">
+                                    <li>
+                                          <img src={figma} alt="Figma icon" />
+                                          <p>Figma</p>
+                                    </li>
+                                    <li>
+                                          <img
+                                                src={illustrator}
+                                                alt="Adobe Illustrator icon"
+                                          />
+                                          <p>Illustrator</p>
+                                    </li>
+                                    <li>
+                                          <img
+                                                src={photoshop}
+                                                alt="Adobe Photoshop icon"
+                                          />
+                                          <p>Photoshop</p>
+                                    </li>
+                              </ul>
+                        </div>
+                        <div className="banner">
+                              <h3>Web</h3>
+                              <ul className="web list">
+                                    <li>
+                                          <img src={html} alt="HTML 5 icon" />
+                                          <p>HTML</p>
+                                    </li>
+                                    <li>
+                                          <img src={css} alt="CSS 3 icon" />
+                                          <p>CSS</p>
+                                    </li>
+                                    <li>
+                                          <img src={js} alt="Javascript icon" />
+                                          <p>Javascript</p>
+                                    </li>
+                              </ul>
+                        </div>
+                        <div className="banner">
+                              <h3>J'apprends</h3>
+                              <ul className="learning list">
+                                    <li>
+                                          <img
+                                                src={react}
+                                                alt="React JS icon"
+                                          />
+                                          <p>React JS</p>
+                                    </li>
+                                    <li>
+                                          <img
+                                                src={react}
+                                                alt="React Native icon"
+                                          />
+                                          <p>React Native</p>
+                                    </li>
+                                    <li>
+                                          <img src={node} alt="Node JS icon" />
+                                          <p>Node JS</p>
+                                    </li>
+                              </ul>
+                        </div>
                   </div>
             </div>
       );
