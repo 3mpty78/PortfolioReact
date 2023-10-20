b2bfc5dc33fcb579ecfdd5dbe2fca9367e36d0a1
3mpty <aurelien.corre@outlook.fr>
2023-10-20 19:34:44 +0200

    Added email button to contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Socials from "../components/Socials";
 
+const email = "aurelien.corre@outlook.fr";
+
 export default function Contact() {
       return (
             <div id="contact">
@@ -8,6 +10,12 @@ export default function Contact() {
                         Me <span>contacter</span>
                   </h2>
                   <div id="underline"></div>
+                  <button
+                        onClick={() => {
+                              window.location.href = `mailto:${email}`;
+                        }}>
+                        email
+                  </button>
                   <div className="contactContainer">
                         <Socials />
                   </div>
