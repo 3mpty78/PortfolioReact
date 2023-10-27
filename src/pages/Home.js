c8e3cc0badfd2dedc7d7532bcb6011c2a89c1b95
3mpty <aurelien.corre@outlook.fr>
2023-10-27 14:28:54 +0200

    Updated fonts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,53 +3,46 @@ import memoji from "../assets/img/memoji_2.png";
 import contact_text from "../assets/icons/contact_icon.svg";
 
 export default function Home() {
-      const [scrollY, setScrollY] = useState(0);
+    const [scrollY, setScrollY] = useState(0);
 
-      useEffect(() => {
-            const handleScroll = () => {
-                  setScrollY(window.scrollY);
-            };
-            window.addEventListener("scroll", handleScroll);
-            return () => {
-                  window.removeEventListener("scroll", handleScroll);
-            };
-      }, []);
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY);
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
-      const textPositionX = -scrollY;
-      const textPositionY = -scrollY;
+    const textPositionX = -scrollY;
+    const textPositionY = -scrollY;
 
-      return (
-            <div id="home">
-                  <a href="#contact">
-                        <img
-                              src={contact_text}
-                              alt="me contacter"
-                              className="contact-text"
-                              style={{
-                                    transform: `rotate(${
-                                          textPositionX / 15
-                                    }deg)`,
-                              }}
-                        />
-                  </a>
-                  <h1
-                        style={{
-                              transform: `translateX(${textPositionX / 2}px)`,
-                        }}>
-                        corre <span>aurélien</span>
-                  </h1>
-                  <h2
-                        style={{
-                              transform: `translateY(${textPositionY / 6}px)`,
-                        }}>
-                        Développeur web front-end
-                  </h2>
-                  <img
-                        className="tronche"
-                        src={memoji}
-                        alt="memoji"
-                        width={350}
-                  />
-            </div>
-      );
+    return (
+        <div id="home">
+            <a href="#contact">
+                <img
+                    src={contact_text}
+                    alt="me contacter"
+                    className="contact-text"
+                    style={{
+                        transform: `rotate(${textPositionX / 15}deg)`,
+                    }}
+                />
+            </a>
+            <h1
+                style={{
+                    transform: `translateX(${textPositionX / 2}px)`,
+                }}>
+                corre <span>aurélien</span>
+            </h1>
+            <h2
+                style={{
+                    transform: `translateY(${textPositionY / 4}px)`,
+                }}>
+                Étudiant développeur intégrateur web
+            </h2>
+            <img className="tronche" src={memoji} alt="memoji" width={350} />
+        </div>
+    );
 }
